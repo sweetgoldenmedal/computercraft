9d55327a859ea7e61086aef82ecec46963884b81
sweetgoldenmedal <flyingmayo+sweetgoldenmedal@gmail.com>
2018-12-29 02:30:09 -0700

    stubs for clear_and_level as well as there_and_back

--- there_and_back.lua
+++ there_and_back.lua
@@ -1,8 +1,35 @@
-distance = args[0] or 20
+local args = {...}
+target_distance = args[1] or 20
 
-if #args != 1
+silent = false
+
+if #args ~= 1
 then
     print("Usage: "..shell.getRunningProgram().." distance_to_mine")
 end
 
-textutils.slowPrint("I will now mine "..distance.." blocks and then mine "..distance.." more blocks on the way back")
+textutils.slowPrint("I will now mine "..target_distance.." blocks and then mine "..target_distance.." more blocks on the way back")
+
+local current_distance = 0
+
+-- outbound trip
+while(current_distance < target_distance)
+do
+    turtle.dig()
+    turtle.forward()
+    current_distance = current_distance + 1
+end
+
+-- turn and move one block left
+turtle.turnLeft()
+turtle.dig()
+turtle.forward()
+turtle.turnLeft()
+
+-- return trip
+while(current_distance > 0)
+do
+    turtle.dig()
+    turtle.forward()
+    current_distance = current_distance - 1
+end
