6f828ac535643345982b26a81ed73c1fa0870936
sweetgoldenmedal <flyingmayo+sweetgoldenmedal@gmail.com>
2018-12-29 02:48:36 -0700

    untested

--- clear_a_square_simple.lua
+++ clear_a_square_simple.lua
@@ -2,9 +2,44 @@ silent = false
 
 local args = {...}
 
-square_size = args[1] or 16
+square_size = args[1] or 8
 
 if #args < 1 then
     print("hey, you forgot to tell me how big of a square you want me to clear!!!")
     error()
+end
+
+textutils.slowPrint("I will clear a square that is "..square_size.." blocks on each side")
+
+current_distance = 0
+current_row = 1
+
+while(current_row < square_size)
+do
+    -- dig a line forward
+    while(current_distance < square_size)
+    do
+        turtle.dig()
+        turtle.forward()
+        current_distance = current_distance + 1
+    end
+
+    -- turn left and dig/move one block forward
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.forward()
+    turtle.turnLeft()
+
+    while(current_distance > square_size)
+    do
+        turtle.dig()
+        turtle.forward()
+        current_distance = current_distance - 1
+    end
+
+    -- now turn right, and restart
+    turtle.turnRight()
+    turtle.dig()
+    turtle.forward()
+    turtle.turnRight()
 end
