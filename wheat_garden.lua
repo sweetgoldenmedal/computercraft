074e7a0b776f27448fc2df0b0ac2cc51b04fd352
sweetgoldenmedal <sweetgoldenmedal@gmail.com>
2019-01-11 23:44:28 -0700

    debug to false rather than 0

--- wheat_garden.lua
+++ wheat_garden.lua
@@ -1,6 +1,6 @@
 -- TODO: grass (and presumably any other obstruction keeps the block from being interacted with
 -- TODO: mobs getting in the way will no doubt foul things up also
-DEBUG = 1
+DEBUG = false 
 -- load up the libraries
 --os.loadAPI("lib/movement")
 os.loadAPI("lib/inventory")
