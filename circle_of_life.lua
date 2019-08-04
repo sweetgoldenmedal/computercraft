5ea1dc059047b1ad69cab88697c5f5da83fec47e
sweetgoldenmedal <sweetgoldenmedal@gmail.com>
2019-08-04 01:46:28 -0700

    damnit

--- circle_of_life.lua
+++ circle_of_life.lua
@@ -1,10 +1,10 @@
-#DEBUG = false
-#os.loadAPI("lib/inventory")
-#os.loadAPI("lib/movement")
+--DEBUG = false
+--os.loadAPI("lib/inventory")
+--os.loadAPI("lib/movement")
 
-# assume that the turtle will move around a square that is 22 on a side
-# assume that the parent turtle has completed the circuit minus one, e.g. the child
-# turtle is placed on the starting square (albeit facing the wrong direction)
+-- assume that the turtle will move around a square that is 22 on a side
+-- assume that the parent turtle has completed the circuit minus one, e.g. the child
+-- turtle is placed on the starting square (albeit facing the wrong direction)
 
 function traverseSide(length)
     length = length or 22
