1c2f3336b8de18bf3c5611c4723668bbb4c1067a
sweetgoldenmedal <sweetgoldenmedal@gmail.com>
2019-08-04 01:20:53 -0700

    bootstrap set to run circle_of_life.lua (which does not yet exist, but this will demonstrate that the bootstrap is an effective method for pushing new birth instructions to turtles without the need to redploy the resourcepack

--- bootstrap_turtle.lua
+++ bootstrap_turtle.lua
@@ -1,1 +1,5 @@
 print("bootstraping turtle")
+
+print("Run The Circle of Life")
+print("This program will cause 2 turtle to perpetually create and destroy eachother")
+shell.run("circle_of_life.lua")
