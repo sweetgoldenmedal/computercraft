4eb1374beaea3a6f21acb5b9cd371511170f4d8d
sweetgoldenmedal <sweetgoldenmedal@gmail.com>
2019-08-04 01:39:27 -0700

    initial commit of circle_of_life

---
+++ circle_of_life.lua
@@ -0,0 +1,24 @@
+#DEBUG = false
+#os.loadAPI("lib/inventory")
+#os.loadAPI("lib/movement"
+
+# assume that the turtle will move around a square that is 22 on a side
+# assume that the parent turtle has completed the circuit minus one, e.g. the child
+# turtle is placed on the starting square (albeit facing the wrong direction)
+
+function traverseSide(length)
+    length = length or 22
+    turtle.turnLeft()
+    for i=1,length do 
+        turtle.dig() # will ensure the parent turtle is always reaped
+        turtle.forward()
+    end
+end
+
+traverseSide()
+traverseSide()
+traverseSide()
+# leave room to give birth
+traverseSide(21)
+# give birth 
+turtle.place()
