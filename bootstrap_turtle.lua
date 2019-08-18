0ce72f5cdab266c4bc0c26a310f677ee83139aaf
scrandell <scrandell@glynlyon.com>
2019-08-18 14:44:58 -0700

    clearing out circle of life

--- bootstrap_turtle.lua
+++ bootstrap_turtle.lua
@@ -1,5 +1,5 @@
 print("bootstraping turtle")
 
-print("Run The Circle of Life")
-print("This program will cause 2 turtles to perpetually create and destroy eachother")
-shell.run("circle_of_life.lua")
+--print("Run The Circle of Life")
+--print("This program will cause 2 turtles to perpetually create and destroy eachother")
+--shell.run("circle_of_life.lua")
