cb086ec4c0439eb1d424e46662ca4f1927b08ed2
root <root@r900.surefall.net>
2019-11-15 23:10:20 -0700

    capturing some resourcepack details and pushing a test change to the bootstrap

--- bootstrap_turtle.lua
+++ bootstrap_turtle.lua
@@ -1,4 +1,5 @@
 print("bootstraping turtle")
+print("adding 2nd instruction")
 
 --print("Run The Circle of Life")
 --print("This program will cause 2 turtles to perpetually create and destroy eachother")
