1b1c13113c9bb5a663ec8bf6b0b1ae2cd274ccea
sweetgoldenmedal <sweetgoldenmedal@gmail.com>
2019-11-16 00:13:06 -0700

    put the dance back in

--- bootstrap_turtle.lua
+++ bootstrap_turtle.lua
@@ -1,5 +1,6 @@
 print("bootstraping turtle")
 print("adding 2nd instruction")
+shell.run("dance")
 --print("Run The Circle of Life")
 --print("This program will cause 2 turtles to perpetually create and destroy eachother")
 --shell.run("circle_of_life.lua")
